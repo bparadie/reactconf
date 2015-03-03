bb3f458d7a80aa4c1ba1000d8b6d187877bf2feb
Bernd Paradies <bernd.paradies@gmail.com>
2015-03-03 20:09:32 -0300

    Remove build files from repo

--- build/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="react/future/react-0.13.0.d.ts" />
-/// <reference path="react/future/react-addons-0.13.0.d.ts" />
-/// <reference path="react/future/react-global-0.13.0.d.ts" />
-/// <reference path="react-jsx/react-jsx.d.ts" />
